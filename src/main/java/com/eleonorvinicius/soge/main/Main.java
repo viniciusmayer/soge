f6d94779236ca2ce23ae29595b178768e9539725
Vinicius Mayer <viniciusmayer@gmail.com>
2016-10-18 22:50:23 -0200

    Project setup.

--- src/main/java/com/eleonorvinicius/soge/main/Main.java
+++ src/main/java/com/eleonorvinicius/soge/main/Main.java
@@ -3,6 +3,7 @@ package com.eleonorvinicius.soge.main;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -16,41 +17,37 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class Main {
 
-	private static final String RS = "RS";
-	private static final String UF = "UF";
 	private static final String FILES_DIRECTORY = "/home/eleonorvinicius/Projects/soge/files/";
 
 	public static void main(String[] args) {
 		File directory = new File(FILES_DIRECTORY);
 		String[] list = directory.list();
+		StringBuilder arquivo = new StringBuilder("");
 		for (String file : list) {
 			Sheet sheet = getSheet(file);
-			Row row = sheet.getRow(1);
-			Integer ufColumn = getUFColumn(row);
 			Iterator<Row> rowIterator = sheet.iterator();
-			int rowCount = 0;
 			while (rowIterator.hasNext()) {
-				row = rowIterator.next();
-				if (rowCount < 2) {
-					rowCount += 1;
-					continue;
-				}
+				Row row = rowIterator.next();
 				StringBuilder linha = new StringBuilder("");
-				Cell cell = row.getCell(ufColumn);
-				if (cell != null){
-					String cellValue = getCellValue(cell);
-					if (cellValue != null && cellValue.equals(RS)){
-						Iterator<Cell> cellIterator = row.cellIterator();
-						while(cellIterator.hasNext()){
-							Cell _cell = cellIterator.next();
-							String s = getCellValue(_cell);
-							linha.append(s);
-							linha.append(" | ");
-						}
-						System.out.println(linha);
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while(cellIterator.hasNext()){
+					Cell cell = cellIterator.next();
+					String s = getCellValue(cell);
+					if (s != null){
+						s = s.replaceAll("[\n\r]", "");
 					}
+					linha.append(s);
+					linha.append(";");
 				}
+				arquivo.append(linha + "\n");
 			}
+		}
+		try {
+			FileWriter fileWriter = new FileWriter("/home/eleonorvinicius/Projects/soge/files/file.csv");
+			fileWriter.write(arquivo.toString());
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
@@ -86,24 +83,5 @@ public class Main {
 			return cell.getStringCellValue();
 		}
 		return null;
-	}
-
-	private static Integer getUFColumn(Row row) {
-		Iterator<Cell> cellIterator = row.cellIterator();
-		Integer ufColumn = null;
-		while (cellIterator.hasNext()) {
-			Cell cell = cellIterator.next();
-			String s = null;
-			switch (cell.getCellType()) {
-			case Cell.CELL_TYPE_STRING:
-				s = cell.getStringCellValue();
-				break;
-			}
-			if (s != null && s.equals(UF)) {
-				ufColumn = cell.getColumnIndex();
-				break;
-			}
-		}
-		return ufColumn;
 	}
 }
